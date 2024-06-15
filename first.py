dc7ff56982d4e6d29735965bd13c621d268c16bf
Priyank264 <priyankec13@gmail.com>
2024-06-15 13:27:12 +0530

    Add files via upload

--- first.py
+++ first.py
@@ -2,4 +2,7 @@ print('hi')
 print('this is issue')
 import streamlit as st
 st.title('priyank patel')
-st.title('ATS Branch')
+st.title('ATS Branc')
+import pandas as pb
+dataset=pb.read_csv("March Probable.csv")
+st.dataframe(dataset)
