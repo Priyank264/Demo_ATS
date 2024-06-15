b6e38a8af6b364fb292ee1be7c6b2f2580093f75
Priyank264 <priyankec13@gmail.com>
2024-06-15 14:26:44 +0530

    Add files via upload

--- first.py
+++ first.py
@@ -2,5 +2,5 @@ import streamlit as st
 st.title('priyank patel')
 st.title('ATS Branch')
 import pandas as pb
-dataset=pb.read_excel("Bill Register.xlsx")
+dataset=pb.read_excel("March Probable.xlsx")
 st.dataframe(dataset)
